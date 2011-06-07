f2e4918bb605a7822dc4b18d2bf63bdad99087e4
darode <darode@bf460b64-c35c-430b-bba4-f971e0999da0>
2011-06-07 11:57:33 +0000

    2011-06-07 Dario Rodriguez <dario.rodriguez@artica.es>
    	*satellite_proxy/cfg.php,
    	satellite_proxy/lib.php,
    	satellite_proxy/satellite_proxy.php,
    	central_proxy/central_proxy.pl,
    	central_proxy/ProxyLib.pm: Files header added, also license and credits
    	of HTTPTunnel by Sebastian Weber (http://http-tunnel.sourceforge.net/)
    
    
    
    git-svn-id: https://svn.code.sf.net/p/epictunnelizer/code/trunk@4 bf460b64-c35c-430b-bba4-f971e0999da0

--- central_proxy/ProxyLib.pm
+++ central_proxy/ProxyLib.pm
@@ -1,3 +1,30 @@
+#    __      _        _____                        _ _              
+#   /__\ __ (_) ___  /__   \_   _ _ __  _ __   ___| (_)_______ _ __ 
+#  /_\| '_ \| |/ __|   / /\/ | | | '_ \| '_ \ / _ \ | |_  / _ \ '__|
+# //__| |_) | | (__   / /  | |_| | | | | | | |  __/ | |/ /  __/ |   
+# \__/| .__/|_|\___|  \/    \__,_|_| |_|_| |_|\___|_|_/___\___|_|   
+#     |_|                                                           
+#
+# http://sourceforge.net/projects/epictunnelizer/
+#
+# ============================================================================
+#
+# This project is an improvement of HTTPTunnelizer by Sebastian Weber, more info
+# at: http://http-tunnel.sourceforge.net/
+#
+# Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+# Please see http://sourceforge.net/projects/epictunnelizer/ for full contribution 
+# list
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; version 2
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 package main;
 
 use threads;
--- central_proxy/central_proxy.pl
+++ central_proxy/central_proxy.pl
@@ -1,3 +1,30 @@
+#    __      _        _____                        _ _              
+#   /__\ __ (_) ___  /__   \_   _ _ __  _ __   ___| (_)_______ _ __ 
+#  /_\| '_ \| |/ __|   / /\/ | | | '_ \| '_ \ / _ \ | |_  / _ \ '__|
+# //__| |_) | | (__   / /  | |_| | | | | | | |  __/ | |/ /  __/ |   
+# \__/| .__/|_|\___|  \/    \__,_|_| |_|_| |_|\___|_|_/___\___|_|   
+#     |_|                                                           
+#
+# http://sourceforge.net/projects/epictunnelizer/
+#
+# ============================================================================
+#
+# This project is an improvement of HTTPTunnelizer by Sebastian Weber, more info
+# at: http://http-tunnel.sourceforge.net/
+#
+# Copyright (c) 2005-2011 Artica Soluciones Tecnologicas
+# Please see http://sourceforge.net/projects/epictunnelizer/ for full contribution 
+# list
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; version 2
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 #!/usr/bin/perl
 
 use warnings;
@@ -705,6 +732,7 @@ sub c_getclientinfo {	# usage: get_getclientinfo servername, serverport
 			mysysread($c_fd,$b,2) or last;
 			@a=unpack("C*",$b);
 			$sp=$a[0]*256+$a[1];
+
 		} else {last;}
 		$msg eq "protocol error" and $msg="";
 		last;
